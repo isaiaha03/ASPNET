382dbe583b14d7089b414335174d68a581e454b5
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-11-18 15:27:12 -0600

    Created Product.cs

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+        public Product() 
+        {
+
+        }
+
+        public int productID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int categoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+    }
+}
